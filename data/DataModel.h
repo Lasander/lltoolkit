4502eb0ceada49d1a0847231baeb75c6fe66e8c2
Lasse Lopperi <lasse.lopperi@iki.fi>
2015-04-26 18:02:34 +0300

    Move definitions out of class. Remove unused getCopy().

--- data/DataModel.h
+++ data/DataModel.h
@@ -15,40 +15,48 @@ namespace Data {
 template <typename T>
 class DataModel : public Publisher<T>
 {
+public:
+    DataModel();
+    DataModel(const T& data);
+    virtual ~DataModel();
+
+    void set(const T& data);
+    const T& get() const;
+
+private:
     T data_;
     Publisher<T> dataPublisher_;
-
-public:
-    DataModel()
-     : data_{}
-    {
-    }
-
-    DataModel(const T& data)
-      : data_(data)
-    {
-    }
-
-    virtual ~DataModel()
-    {
-    }
-
-    void set(const T& data)
-    {
-        data_ = data;
-        Publisher<T>::notifySubscribers(data_);
-    }
-
-    const T& get() const
-    {
-        return data_;
-    }
-
-    T getCopy() const
-    {
-        return data_;
-    }
 };
+
+template <typename T>
+DataModel<T>::DataModel()
+ : data_{}
+{
+}
+
+template <typename T>
+DataModel<T>::DataModel(const T& data)
+  : data_(data)
+{
+}
+
+template <typename T>
+DataModel<T>::~DataModel()
+{
+}
+
+template <typename T>
+void DataModel<T>::set(const T& data)
+{
+    data_ = data;
+    Publisher<T>::notifySubscribers(data_);
+}
+
+template <typename T>
+const T& DataModel<T>::get() const
+{
+    return data_;
+}
 
 }  // namespace Data
 
